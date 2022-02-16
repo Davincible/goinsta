1436a29b6e70d36c103efbae1dbb9f328917d200
Davincible <david.brouwer.99@gmail.com>
2022-02-16 18:47:53 +0100

    Start to refactor Users struct

--- users.go
+++ users.go
@@ -117,88 +117,108 @@ type User struct {
 	insta       *Instagram
 	Collections *Collections
 
-	ID                         int64         `json:"pk"`
-	Username                   string        `json:"username"`
-	FullName                   string        `json:"full_name"`
-	Biography                  string        `json:"biography"`
-	BestiesCount               int           `json:"besties_count"`
-	ShowBestiesBadge           bool          `json:"show_besties_badge"`
-	RecentlyBestiedByCount     int           `json:"recently_bestied_by_count"`
-	ProfilePicURL              string        `json:"profile_pic_url"`
-	Email                      string        `json:"email"`
-	PhoneNumber                string        `json:"phone_number"`
-	WhatsappNumber             string        `json:"whatsapp_number"`
-	IsBusiness                 bool          `json:"is_business"`
-	AccountType                int           `json:"account_type"`
-	AccountBadges              []interface{} `json:"account_badges"`
-	Gender                     int           `json:"gender"`
-	ProfilePicID               string        `json:"profile_pic_id"`
-	FbIdV2                     int64         `json:"fbid_v2"`
-	HasAnonymousProfilePicture bool          `json:"has_anonymous_profile_picture"`
-	IsPrivate                  bool          `json:"is_private"`
-	IsUnpublished              bool          `json:"is_unpublished"`
-	IsMutedWordsGlobalEnabled  bool          `json:"is_muted_words_global_enabled"`
-	IsMutedWordsCustomEnabled  bool          `json:"is_muted_words_custom_enabled"`
-	AllowedCommenterType       string        `json:"allowed_commenter_type"`
-	UserTagsCount              int           `json:"usertags_count"`
-	UserTagReviewEnabled       bool          `json:"usertag_review_enabled"`
-	IsVerified                 bool          `json:"is_verified"`
-	IsNeedy                    bool          `json:"is_needy"`
-	IsInterestAccount          bool          `json:"is_interest_account"`
-	IsVideoCreator             bool          `json:"is_video_creator"`
-	MediaCount                 int           `json:"media_count"`
-	IGTVCount                  int           `json:"total_igtv_videos"`
-	HasIGTVSeries              bool          `json:"has_igtv_series"`
-	TotalClipCount             int           `json:"total_clips_count"`
-	TotalAREffects             int           `json:"total_ar_effects"`
-	FollowerCount              int           `json:"follower_count"`
-	FollowingCount             int           `json:"following_count"`
-	FollowingTagCount          int           `json:"following_tag_count"`
-	MutualFollowersID          []int64       `json:"profile_context_mutual_follow_ids"`
-	FollowFrictionType         int           `json:"follow_friction_type"`
-	ProfileContext             string        `json:"profile_context"`
-	GeoMediaCount              int           `json:"geo_media_count"`
-	ExternalURL                string        `json:"external_url"`
-	HasBiographyTranslation    bool          `json:"has_biography_translation"`
-	HasVideos                  bool          `json:"has_videos"`
-	HasProfileVideoFeed        bool          `json:"has_profile_video_feed"`
-	HasSavedItems              bool          `json:"has_saved_items"`
-	ExternalLynxURL            string        `json:"external_lynx_url"`
+	// User info
+	ID                     int64  `json:"pk"`
+	Username               string `json:"username"`
+	FullName               string `json:"full_name"`
+	Email                  string `json:"email"`
+	PhoneNumber            string `json:"phone_number"`
+	WhatsappNumber         string `json:"whatsapp_number"`
+	Gender                 int    `json:"gender"`
+	PublicEmail            string `json:"public_email"`
+	PublicPhoneNumber      string `json:"public_phone_number"`
+	PublicPhoneCountryCode string `json:"public_phone_country_code"`
+	ContactPhoneNumber     string `json:"contact_phone_number"`
+
+	// Profile visible properties
+	IsPrivate                  bool   `json:"is_private"`
+	IsVerified                 bool   `json:"is_verified"`
+	ExternalURL                string `json:"external_url"`
+	ExternalLynxURL            string `json:"external_lynx_url"`
+	FollowerCount              int    `json:"follower_count"`
+	FollowingCount             int    `json:"following_count"`
+	ProfilePicID               string `json:"profile_pic_id"`
+	ProfilePicURL              string `json:"profile_pic_url"`
+	HasAnonymousProfilePicture bool   `json:"has_anonymous_profile_picture"`
+	Biography                  string `json:"biography"`
 	BiographyWithEntities      struct {
 		RawText  string        `json:"raw_text"`
 		Entities []interface{} `json:"entities"`
 	} `json:"biography_with_entities"`
-	BiographyProductMentions       []interface{} `json:"biography_products_mentions"`
+	BiographyProductMentions []interface{} `json:"biography_products_mentions"`
+
+	// Profile hidden properties
+	IsNeedy                        bool          `json:"is_needy"`
+	IsInterestAccount              bool          `json:"is_interest_account"`
+	IsVideoCreator                 bool          `json:"is_video_creator"`
+	IsBusiness                     bool          `json:"is_business"`
+	BestiesCount                   int           `json:"besties_count"`
+	ShowBestiesBadge               bool          `json:"show_besties_badge"`
+	RecentlyBestiedByCount         int           `json:"recently_bestied_by_count"`
+	AccountType                    int           `json:"account_type"`
+	AccountBadges                  []interface{} `json:"account_badges"`
+	FbIdV2                         int64         `json:"fbid_v2"`
+	IsUnpublished                  bool          `json:"is_unpublished"`
+	UserTagsCount                  int           `json:"usertags_count"`
+	UserTagReviewEnabled           bool          `json:"usertag_review_enabled"`
+	FollowingTagCount              int           `json:"following_tag_count"`
+	MutualFollowersID              []int64       `json:"profile_context_mutual_follow_ids"`
+	FollowFrictionType             int           `json:"follow_friction_type"`
+	ProfileContext                 string        `json:"profile_context"`
+	HasBiographyTranslation        bool          `json:"has_biography_translation"`
+	HasSavedItems                  bool          `json:"has_saved_items"`
 	Nametag                        Nametag       `json:"nametag"`
 	HasChaining                    bool          `json:"has_chaining"`
-	HasPlacedOrders                bool          `json:"has_placed_orders"`
 	IsFavorite                     bool          `json:"is_favorite"`
 	IsFavoriteForStories           bool          `json:"is_favorite_for_stories"`
 	IsFavoriteForHighlights        bool          `json:"is_favorite_for_highlights"`
 	IsProfileActionNeeded          bool          `json:"is_profile_action_needed"`
 	CanBeReportedAsFraud           bool          `json:"can_be_reported_as_fraud"`
-	CanBoostPosts                  bool          `json:"can_boost_posts"`
-	CanSeeOrganicInsights          bool          `json:"can_see_organic_insights"`
-	CanConvertToBusiness           bool          `json:"can_convert_to_business"`
-	CanCreateSponsorTags           bool          `json:"can_create_sponsor_tags"`
-	CanCreateNewFundraiser         bool          `json:"can_create_new_standalone_fundraiser"`
-	CanCreateNewPersonalFundraiser bool          `json:"can_create_new_standalone_personal_fundraiser"`
-	CanBeTaggedAsSponsor           bool          `json:"can_be_tagged_as_sponsor"`
 	CanSeeSupportInbox             bool          `json:"can_see_support_inbox"`
 	CanSeeSupportInboxV1           bool          `json:"can_see_support_inbox_v1"`
-	CanTagProductsFromMerchants    bool          `json:"can_tag_products_from_merchants"`
 	CanSeePrimaryCountryInsettings bool          `json:"can_see_primary_country_in_settings"`
 	CanFollowHashtag               bool          `json:"can_follow_hashtag"`
-	PersonalAccountAdsPageName     string        `json:"personal_account_ads_page_name"`
-	PersonalAccountAdsId           string        `json:"personal_account_ads_page_id"`
-	ShowShoppableFeed              bool          `json:"show_shoppable_feed"`
-	ShowInsightTerms               bool          `json:"show_insights_terms"`
-	ShowConversionEditEntry        bool          `json:"show_conversion_edit_entry"`
-	ShowPostsInsightEntryPoint     bool          `json:"show_post_insights_entry_point"`
-	ShoppablePostsCount            int           `json:"shoppable_posts_count"`
-	RequestContactEnabled          bool          `json:"request_contact_enabled"`
-	FeedPostReshareDisabled        bool          `json:"feed_post_reshare_disabled"`
-	CreatorShoppingInfo            struct {
+
+	// Business profile properies
+	CanBoostPosts                  bool   `json:"can_boost_posts"`
+	CanSeeOrganicInsights          bool   `json:"can_see_organic_insights"`
+	CanConvertToBusiness           bool   `json:"can_convert_to_business"`
+	CanCreateSponsorTags           bool   `json:"can_create_sponsor_tags"`
+	CanCreateNewFundraiser         bool   `json:"can_create_new_standalone_fundraiser"`
+	CanCreateNewPersonalFundraiser bool   `json:"can_create_new_standalone_personal_fundraiser"`
+	CanBeTaggedAsSponsor           bool   `json:"can_be_tagged_as_sponsor"`
+	PersonalAccountAdsPageName     string `json:"personal_account_ads_page_name"`
+	PersonalAccountAdsId           string `json:"personal_account_ads_page_id"`
+	Category                       string `json:"category"`
+
+	// Shopping properties
+	ShowShoppableFeed           bool `json:"show_shoppable_feed"`
+	CanTagProductsFromMerchants bool `json:"can_tag_products_from_merchants"`
+
+	// Miscellaneous
+	IsMutedWordsGlobalEnabled bool   `json:"is_muted_words_global_enabled"`
+	IsMutedWordsCustomEnabled bool   `json:"is_muted_words_custom_enabled"`
+	AllowedCommenterType      string `json:"allowed_commenter_type"`
+
+	// Media properties
+	MediaCount          int  `json:"media_count"`
+	IGTVCount           int  `json:"total_igtv_videos"`
+	HasIGTVSeries       bool `json:"has_igtv_series"`
+	HasVideos           bool `json:"has_videos"`
+	TotalClipCount      int  `json:"total_clips_count"`
+	TotalAREffects      int  `json:"total_ar_effects"`
+	GeoMediaCount       int  `json:"geo_media_count"`
+	HasProfileVideoFeed bool `json:"has_profile_video_feed"`
+
+	HasPlacedOrders bool `json:"has_placed_orders"`
+
+	ShowInsightTerms           bool `json:"show_insights_terms"`
+	ShowConversionEditEntry    bool `json:"show_conversion_edit_entry"`
+	ShowPostsInsightEntryPoint bool `json:"show_post_insights_entry_point"`
+	ShoppablePostsCount        int  `json:"shoppable_posts_count"`
+	RequestContactEnabled      bool `json:"request_contact_enabled"`
+	FeedPostReshareDisabled    bool `json:"feed_post_reshare_disabled"`
+	CreatorShoppingInfo        struct {
 		LinkedMerchantAccounts []interface{} `json:"linked_merchant_accounts"`
 	} `json:"creator_shopping_info"`
 	StandaloneFundraiserInfo struct {
@@ -220,17 +240,13 @@ type User struct {
 	HightlightReshareDisabled    bool         `json:"highlight_reshare_disabled"`
 	IsMemorialized               bool         `json:"is_memorialized"`
 	HasGuides                    bool         `json:"has_guides"`
-	PublicEmail                  string       `json:"public_email"`
-	PublicPhoneNumber            string       `json:"public_phone_number"`
-	PublicPhoneCountryCode       string       `json:"public_phone_country_code"`
-	ContactPhoneNumber           string       `json:"contact_phone_number"`
+	HasAffiliateShop             bool         `json:"has_active_affiliate_shop"`
 	CityID                       int64        `json:"city_id"`
 	CityName                     string       `json:"city_name"`
 	AddressStreet                string       `json:"address_street"`
 	DirectMessaging              string       `json:"direct_messaging"`
 	Latitude                     float64      `json:"latitude"`
 	Longitude                    float64      `json:"longitude"`
-	Category                     string       `json:"category"`
 	BusinessContactMethod        string       `json:"business_contact_method"`
 	IncludeDirectBlacklistStatus bool         `json:"include_direct_blacklist_status"`
 	HdProfilePicURLInfo          PicURLInfo   `json:"hd_profile_pic_url_info"`
@@ -256,9 +272,11 @@ type User struct {
 	ElegibleShoppingSignupEntrypoints          []interface{} `json:"eligible_shopping_signup_entrypoints"`
 	IsIgdProductPickerEnabled                  bool          `json:"is_igd_product_picker_enabled"`
 	IsElegibleForAffiliateShopOnboarding       bool          `json:"is_eligible_for_affiliate_shop_onboarding"`
+	IsElegibleForSMBSupportFlow                bool          `json:"is_eligible_for_smb_support_flow"`
 	ElegibleShoppingFormats                    []interface{} `json:"eligible_shopping_formats"`
 	NeedsToAcceptShoppingSellerOnboardingTerms bool          `json:"needs_to_accept_shopping_seller_onboarding_terms"`
 	SellerShoppableFeedType                    string        `json:"seller_shoppable_feed_type"`
+	VisibleProducts                            int           `json:"num_visible_storefront_products"`
 	IsShoppingCatalogSettingsEnabled           bool          `json:"is_shopping_settings_enabled"`
 	IsShoppingCommunityContentEnabled          bool          `json:"is_shopping_community_content_enabled"`
 	IsShoppingAutoHighlightEnabled             bool          `json:"is_shopping_auto_highlight_eligible"`
@@ -270,6 +288,7 @@ type User struct {
 	ExistingUserAgeCollectionEnabled           bool          `json:"existing_user_age_collection_enabled"`
 	AboutYourAccountBloksEntrypointEnabled     bool          `json:"about_your_account_bloks_entrypoint_enabled"`
 	OpenExternalUrlWithInAppBrowser            bool          `json:"open_external_url_with_in_app_browser"`
+	MerchantCheckoutStyle                      string        `json:"merchant_checkout_style"`
 
 	// Profile picture as raw bytes, to populate call User.DownloadProfilePic()
 	ProfilePic []byte
